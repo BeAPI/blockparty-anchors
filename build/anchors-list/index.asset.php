9f1b58c7c00e324e21806c18fa45d8e923aa280b
stephane-gillot <44164024+stephane-gillot@users.noreply.github.com>
2026-09-22 11:37:27 +0000

    Release version 1.0.3

---
+++ build/anchors-list/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '508451a3bf8187f8f898');
---
+++ build/anchors-list/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '156cb1cebd1aaf2eb403');
